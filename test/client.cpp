45ce0ac2bc63156bdd78159bc1ad11a69ec4e633
fantasy-peak <1356346239@qq.com>
2025-05-26 16:48:55 +0800

    Add HttpsClient

---
+++ test/client.cpp
@@ -0,0 +1,101 @@
+#include <chrono>
+#include <cstdio>
+#include <memory>
+#include <ostream>
+#include <string>
+
+#include <boost/beast/core/string_type.hpp>
+#include <variant>
+#include "boost/asio/as_tuple.hpp"
+#include "boost/asio/awaitable.hpp"
+#include "boost/asio/co_spawn.hpp"
+#include "boost/asio/detached.hpp"
+#include "boost/asio/use_awaitable.hpp"
+
+#include "simple_http.h"
+
+namespace asio = boost::asio;
+namespace beast = boost::beast;
+namespace http = beast::http;
+
+// export SSL_CERT_FILE=/etc/ssl/certs/ca-certificates.crt
+asio::awaitable<void> client()
+{
+    static simple_http::IoCtxPool pool{2};
+    pool.start();
+
+    asio::steady_timer timer(*pool.getMainContext());
+    timer.expires_after(std::chrono::seconds(3));
+    co_await timer.async_wait();
+
+    std::shared_ptr<simple_http::HttpClient> client =
+        std::make_shared<simple_http::HttpsClient>("learnrust.site",
+                                                   6666,
+                                                   pool.getIoContextPtr());
+    assert(co_await client->start());
+
+    auto ctx = pool.getMainContext();
+    asio::co_spawn(
+        *ctx,
+        [=]() -> asio::awaitable<void> {
+            auto req = std::make_shared<http::request<http::string_body>>(
+                http::verb::post, "/hello", 11);
+            req->set(http::field::user_agent, "my-client");
+            req->set("X-Custom-Header", "value");
+            req->body() = "client";
+            auto ch = co_await client->sendRequest(req);
+            std::cout << "recv http\n";
+            for (;;)
+            {
+                auto [ec, data] = co_await ch->async_receive(
+                    asio::as_tuple(asio::use_awaitable));
+                if (ec)
+                {
+                    std::cout << ec.message() << std::endl;
+                    break;
+                }
+                if (std::holds_alternative<simple_http::Disconnect>(data))
+                {
+                    std::cout << "Disconnect" << std::endl;
+                    break;
+                }
+                else if (std::holds_alternative<std::string>(data))
+                {
+                    auto body_str = std::get<std::string>(data);
+                    std::cout << "recv body:" << body_str << std::endl;
+                }
+                else
+                {
+                    auto res = std::get<http::response<http::empty_body>>(data);
+                    for (const auto &field : res)
+                    {
+                        std::cout << field.name_string() << ": "
+                                  << field.value() << "\n";
+                    }
+                }
+            }
+        },
+        asio::detached);
+
+    timer.expires_after(std::chrono::seconds(30));
+    co_await timer.async_wait();
+
+    co_await client->stop();
+
+    co_return;
+}
+
+int main()
+{
+    simple_http::LOG_CB =
+        [](simple_http::LogLevel level, auto file, auto line, std::string msg) {
+            std::cout << to_string(level) << " " << file << ":" << line << " "
+                      << msg << std::endl;
+        };
+    simple_http::IoCtxPool pool{1};
+    pool.start();
+    asio::co_spawn(pool.getIoContext(), client(), asio::detached);
+    while (true)
+        sleep(1000);
+    return 0;
+}
--- test/server.cpp
+++ test/server.cpp
@@ -1,11 +1,15 @@
 #include <chrono>
 #include <cstdio>
+#include <exception>
+#include <memory>
 #include <ostream>
 #include <string>
 #include <syncstream>
 
 #include <boost/beast/core/string_type.hpp>
 
+#include "boost/asio/as_tuple.hpp"
+#include "boost/asio/use_awaitable.hpp"
 #include "simple_http.h"
 
 namespace asio = boost::asio;
@@ -20,7 +24,7 @@ asio::awaitable<void> start()
                             .concurrent_streams = 200};
     cfg.ssl_crt = "./v.crt";
     cfg.ssl_key = "./v.key";
-    static simple_http::HttpServer hs(cfg);
+    simple_http::HttpServer hs(cfg);
     simple_http::LOG_CB =
         [](simple_http::LogLevel level, auto file, auto line, std::string msg) {
             std::osyncstream out(std::cout);
@@ -29,19 +33,19 @@ asio::awaitable<void> start()
         };
     hs.setHttpHandler(
         "/hello", [](auto req, auto writer) -> asio::awaitable<void> {
-            std::osyncstream out(std::cout);
-            out << "Headers:\n";
+            std::cout << "Headers:" << std::endl;
             // std::println("meth: {}", std::string{req.method_string()});
             for (auto const &field : req)
             {
-                out << field.name_string() << ": " << field.value() << "\n";
+                std::cout << field.name_string() << ": " << field.value()
+                          << "\n";
             }
-            out << req.target() << std::endl;
+            std::cout << req.target() << std::endl;
             auto str = req.body();
-            out << "body:" << str << std::endl;
+            std::cout << "body:" << str << std::endl;
             if (writer->version() == simple_http::Version::Http2)
             {
-#if 1
+#if 0
                 http::response<http::string_body> res;
                 res.result(http::status::ok);
                 res.set(http::field::content_type, "text/plain");
@@ -50,19 +54,17 @@ asio::awaitable<void> start()
                 writer->writeHttpResponse(
                     std::make_shared<http::response<http::string_body>>(res));
 #else
-                size_t total = str.size();
-                size_t part_size = total / 3;
-
-                std::string part1 = str.substr(0, part_size);
-                std::string part2 = str.substr(part_size, part_size);
-                std::string part3 = str.substr(part_size * 2);
-
                 writer->writeHeader("content-type", "text/plain");
                 writer->writeHeader(http::field::server, "test");
                 writer->writeHeaderEnd();
-                writer->writeBody(part1);
-                writer->writeBody(part2);
-                writer->writeBodyEnd(part3);
+                writer->writeBody("123");
+                asio::steady_timer timer(co_await asio::this_coro::executor);
+                timer.expires_after(std::chrono::seconds(4));
+                co_await timer.async_wait(asio::use_awaitable);
+                writer->writeBody("456");
+                timer.expires_after(std::chrono::seconds(4));
+                co_await timer.async_wait(asio::use_awaitable);
+                writer->writeBodyEnd("789");
 #endif
             }
             else
@@ -71,7 +73,7 @@ asio::awaitable<void> start()
                 http::response<http::string_body> res;
                 res.version(11);
                 res.result(http::status::ok );
-                res.set(http::field::server, "MyBeastServer");
+                res.set(http::field::server, "simple_http_server");
                 res.set(http::field::content_type, "text/plain");
                 res.body() = "hello world";
                 res.prepare_payload();
@@ -80,7 +82,7 @@ asio::awaitable<void> start()
 #endif
                 // curl --no-buffer  -v http://localhost:6666/hello -d "aaaa"
                 http::response<http::empty_body> res{http::status::ok, 11};
-                res.set(http::field::server, "BeastChunkedServer/1.0");
+                res.set(http::field::server, "simple_http_server");
                 res.set(http::field::content_type, "text/plain");
                 res.set(http::field::transfer_encoding, "chunked");  // 关键字段
                 res.keep_alive(true);
@@ -93,6 +95,32 @@ asio::awaitable<void> start()
                 timer.expires_after(std::chrono::seconds(4));
                 co_await timer.async_wait(asio::use_awaitable);
                 writer->writeChunkEnd();
+            }
+            co_return;
+        });
+    hs.setHttpHandler(
+        "/world", [](auto /* req */, auto writer) -> asio::awaitable<void> {
+            if (writer->version() == simple_http::Version::Http2)
+            {
+                http::response<http::string_body> res;
+                res.result(http::status::ok);
+                res.set(http::field::content_type, "text/plain");
+                res.body() = "/world http2 body";
+                res.prepare_payload();
+                writer->writeHttpResponse(
+                    std::make_shared<http::response<http::string_body>>(res));
+            }
+            else
+            {
+                http::response<http::string_body> res;
+                res.version(11);
+                res.result(http::status::ok);
+                res.set(http::field::server, "simple_http_server");
+                res.set(http::field::content_type, "text/plain");
+                res.body() = "/world http1.1 body";
+                res.prepare_payload();
+                writer->writeHttpResponse(
+                    std::make_shared<http::response<http::string_body>>(res));
             }
             co_return;
         });
